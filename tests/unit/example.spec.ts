db743fcb8816bd519374a22472e0d93ea4dc924e
Kamina <segmentree@gmail.com>
2020-08-17 23:07:16 -0400

    Configure vuetify + jest + TS

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,11 +1,17 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
 
 describe('HelloWorld.vue', () => {
+  let wrapper
+  beforeEach(() => {
+    Vue.use(Vuetify)
+  })
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+    const msg = 'Welcome to Vuetify'
+    wrapper = shallowMount(HelloWorld, {
+      propsData: { msg },
     })
     expect(wrapper.text()).toMatch(msg)
   })
